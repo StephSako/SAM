035e165ef6c8c402925611700092cd585989edd5
vincent faury <vincent.faury@gmail.com>
2020-11-30 15:37:03 +0100

    Sign-Up fini

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },  {
+    path: 'sign-up',
+    loadChildren: () => import('./sign-up/sign-up.module').then( m => m.SignUpPageModule)
   }
+
 
 
 ];
