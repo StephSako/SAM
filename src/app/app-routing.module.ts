63b2998dcfe0fcc7d3cad9394c24a6b326d42c97
PinsardF <mysteries@orange.fr>
2020-12-03 15:42:47 +0100

    confirmation done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },  {
+  },
+  {
     path: 'account',
     loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
   },
   {
     path: 'course-ongoing',
     loadChildren: () => import('./course-ongoing/course-ongoing.module').then( m => m.CourseOngoingPageModule)
+  },
+  {
+    path: 'confirm',
+    loadChildren: () => import('./course-confirmation/course-confirmation.module').then( m => m.CourseConfirmationPageModule)
   }
 
 ];
