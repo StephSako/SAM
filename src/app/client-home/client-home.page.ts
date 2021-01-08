2eca30d9510d2b51e009d2206d5c22b950ce3c61
carage34 <jully.morgan@orange.fr>
2021-01-08 14:41:54 +0100

    display user and toast fix

--- src/app/client-home/client-home.page.ts
+++ src/app/client-home/client-home.page.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Capacitor, Plugins, GeolocationPosition } from '@capacitor/core';
 import { Observable, of, from as fromPromise } from 'rxjs';
 import { tap, map, switchMap } from 'rxjs/operators';
 import { LoadingController, AlertController } from '@ionic/angular';
+import { UserInterface } from '../Interfaces/UserInterface';
+import { AuthService } from '../services/auth.service';
+import { Appearance } from '@angular-material-extensions/google-maps-autocomplete';
+import { DriverData } from '../tab1/driver-data.model';
+import PlaceResult = google.maps.places.PlaceResult;
 
 const { Toast, Geolocation } = Capacitor.Plugins;
 
@@ -10,20 +15,44 @@ const { Toast, Geolocation } = Capacitor.Plugins;
   selector: 'app-client-home',
   templateUrl: './client-home.page.html',
   styleUrls: ['./client-home.page.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ClientHomePage implements OnInit {
 
   public coordinates: Observable<GeolocationPosition>;
+  user: UserInterface;
   public defaultPos: {
     lattitude: 45,
     longitude: 9
   };
 
-  constructor(public loading: LoadingController, public alertCtrl: AlertController) { }
+  public appearance = Appearance;
+  public zoom: number;
+  public latitude: number;
+  public longitude: number;
+  public selectedAddress: PlaceResult;
+  public map: google.maps.Map
+  private drivers: DriverData[];
+
+  public lat;
+  public lon;
+  public bounds;
+
+  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef
+
+  constructor(public loading: LoadingController, public alertCtrl: AlertController, private authService: AuthService) { }
 
   ngOnInit() {
+    this.user = this.authService.getUserDetails();
+    this.authService
+    .getDrivers()
+    .subscribe((drivers: DriverData[]) => {
+      this.drivers = drivers;
+    })
     /**/
     // start the loader
+    this.lat = 45;
+    this.lon = 9
     this.displayLoader()
       .then((loader: any) => {
         // get position
@@ -31,6 +60,10 @@ export class ClientHomePage implements OnInit {
           .then(position => {
             //close loader and return position
             loader.dismiss();
+            this.lat = position.coords.latitude;
+            this.lon = position.coords.longitude;
+            this.initMap();
+            //this.placeDriverMarker();
             return position;
           })
           // if error
@@ -92,4 +125,47 @@ export class ClientHomePage implements OnInit {
     return POSITION;
   }
 
+  placeDriverMarker() {
+    console.log("drivers");
+    console.log(this.drivers);
+    this.drivers.forEach(driver => {
+      //console.log(driver);
+      if ((driver.longitude_pos) && (driver.latitude_pos)) {
+        console.log(driver.latitude_pos);
+        let msg = "<br/> <b>" + driver.firstname + " " + driver.lastname + " à 3km <br/>"
+        let info = new google.maps.InfoWindow({
+          content: msg
+        })
+        let marker = new google.maps.Marker({
+          position: { lat: driver.latitude_pos, lng: driver.longitude_pos },
+          map: this.map,
+          icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
+        })
+        marker.addListener("click", () => {
+          info.open(this.map, marker);
+        })
+      }
+    })
+  }
+
+  initMap() {
+    var input = document.getElementById('pac-input') as HTMLInputElement;
+    //let places = new google.maps.places.PlacesServices(this.map)
+    const POSITION = {
+      lat: this.lat,
+      lng: this.lon
+    }
+    this.map = new google.maps.Map(
+      document.getElementById("map"),
+      {
+        zoom: 12,
+        center: POSITION || { lat: 22, lng: 22 },
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        streetViewControl: false,
+        disableDefaultUI: true,
+      }
+    );
+    const callButton = document.getElementById("call");
+    this.map.controls[google.maps.ControlPosition.BOTTOM_CENTER].push(callButton);
+  }
 }
